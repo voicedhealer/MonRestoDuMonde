1d3306956847e681af08971cced5776d5f5aba8b
voicedhealer <vivien.bernardot@gmail.com>
2019-01-14 11:17:09 +0100

    TableBarCustom

---
+++ MonRestoDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,15 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by bernardot vivien on 14/01/2019.
+//  Copyright © 2019 bernardot vivien. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
+
+
---
+++ MonRestoDuMonde/controlleur/MonTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  MonTabBarController.swift
+//  MonRestoDuMonde
+//
+//  Created by bernardot vivien on 14/01/2019.
+//  Copyright © 2019 bernardot vivien. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+       
+    }
+
+}
