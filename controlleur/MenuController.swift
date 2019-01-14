7784ca485d1077a1d962b694a53078bfb4f2f27d
voicedhealer <vivien.bernardot@gmail.com>
2019-01-14 10:58:20 +0100

    Ajout des controller principaux

---
+++ controlleur/MenuController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by bernardot vivien on 14/01/2019.
+//  Copyright © 2019 bernardot vivien. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
