92f2acd1a056f741c4da2f773940de633e876e7a
voicedhealer <vivien.bernardot@gmail.com>
2019-01-18 17:10:21 +0100

    Ajout d'un menuCell dens le StoryBoard

--- MonRestoDuMonde/controlleur/MenuController.swift
+++ MonRestoDuMonde/controlleur/MenuController.swift
@@ -35,8 +35,9 @@ class MenuController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let menu = menus[indexPath.item]
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellId, for: indexPath) as? MenuCell {
-            cell.backgroundColor = GRIS_TRES_FONCE
+            cell.miseEnPlace(menu: menu)
             return cell
         }
         return UICollectionViewCell()
